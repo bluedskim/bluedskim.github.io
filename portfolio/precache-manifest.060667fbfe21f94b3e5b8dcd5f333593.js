f9f9d0247168a9b4f106415cf53d7f80d839a058
dskim <bluedskim@gmail.com>
2021-04-22 00:55:04 +0900

    add portfolio

---
+++ portfolio/precache-manifest.060667fbfe21f94b3e5b8dcd5f333593.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afa1e439f8cf0aedddc48571d48dfcdd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8db857c4cea0fe3f9a18",
+    "url": "/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "dd6511acbd4e096c3105",
+    "url": "/static/js/2.992c44dd.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.992c44dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8db857c4cea0fe3f9a18",
+    "url": "/static/js/main.a3d23e01.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "bc324fd03ea0a2b34ee7f66f1f08724d",
+    "url": "/static/media/finmart1.bc324fd0.png"
+  },
+  {
+    "revision": "418a11d0f20771cf83334bc8bd0d8de3",
+    "url": "/static/media/finmartLogo2.418a11d0.png"
+  },
+  {
+    "revision": "6cbf91c4bf607568d9c5e51412d44fba",
+    "url": "/static/media/kclLogo.6cbf91c4.png"
+  },
+  {
+    "revision": "0b26f5d3e8037d625c6f4c2a3cdfad8d",
+    "url": "/static/media/kcl_togo1.0b26f5d3.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/static/media/resume.9d3da0e2.pdf"
+  },
+  {
+    "revision": "79ea67d3a82afae1466b64ea36509bad",
+    "url": "/static/media/내얼굴 일러스트.79ea67d3.jpeg"
+  }
+]);
